b6f98b2241ebd9b7ad3cccb901408efdefe96994
XCJ <i@xcj.im>
2024-08-20 00:50:55 +0800

    feat:  还原 skill 图标引入方式

--- src/config/skill.ts
+++ src/config/skill.ts
@@ -1,4 +1,9 @@
 import { Html5, Css3Alt, JsSquare, GitAlt, Github } from '@vicons/fa'
+import Typescript from '@/assets/svg/Typescript-logo.svg'
+import Vue from '@/assets/svg/Vue-logo.svg'
+import VueUse from '@/assets/svg/VueUse-logo.svg'
+import Vite from '@/assets/svg/Vite-logo.svg'
+import Threejs from '@/assets/svg/Threejs-logo.svg'
 
 interface Skill {
   name: string
@@ -36,27 +41,27 @@ export const skillList: Array<Skill_Icon | Skill_Image> = [
   },
   {
     name: 'TypeScript',
-    icon: new URL('@/assets/svg/Typescript-logo.svg', import.meta.url).href,
+    icon: Typescript,
     type: 'image',
   },
   {
     name: 'Vue.js',
-    icon: new URL('@/assets/svg/Vue-logo.svg', import.meta.url).href,
+    icon: Vue,
     type: 'image',
   },
   {
     name: 'VueUse',
-    icon: new URL('@/assets/svg/VueUse-logo.svg', import.meta.url).href,
+    icon: VueUse,
     type: 'image',
   },
   {
     name: 'Vite',
-    icon: new URL('@/assets/svg/Vite-logo.svg', import.meta.url).href,
+    icon: Vite,
     type: 'image',
   },
   {
     name: 'Three.js',
-    icon: new URL('@/assets/svg/Threejs-logo.svg', import.meta.url).href,
+    icon: Threejs,
     type: 'image',
   },
   {
