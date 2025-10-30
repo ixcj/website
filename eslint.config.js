d58c71eb04e80f7e9e8867753192cc23c562518c
XCJ <i@xcj.im>
2025-10-30 11:59:09 +0800

    style: 格式化 vite.config.ts

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   rules: {
     'no-console': 'off',
-    'vue/require-v-for-key':  'off',
+    'vue/require-v-for-key': 'off',
+    '@typescript-eslint/no-require-imports': 'off',
   },
 })
