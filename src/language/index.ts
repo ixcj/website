3c5cf85730483c24a4f53b4dcac589626523cbab
XCJ <i@xcj.im>
2025-12-11 00:05:27 +0800

    v0.1.3

--- src/language/index.ts
+++ src/language/index.ts
@@ -1,14 +1,12 @@
 import { createI18n } from 'vue-i18n'
-import en from './lang/en.ts'
-import zh from './lang/zh.ts'
+import { defaultLanguage, messages } from './messages'
 
 // @ts-expect-error 禁用开发者工具支持
 globalThis.__VUE_PROD_DEVTOOLS__ = false
 
-export const defaultLanguage = 'zh'
-export const messages = { zh, en }
+const baseUrl = import.meta.env.BASE_URL
 
-const locale = globalThis.location?.pathname.replace(/^\/|\/$/g, '')
+const locale = getLang(globalThis.location?.pathname, baseUrl)
   || defaultLanguage
 const i18n = createI18n({
   locale,
@@ -20,4 +18,21 @@ const i18n = createI18n({
 export default i18n
 
 export const mottoLength
-  = locale === 'zh' ? zh.mottos.length : en.mottos.length
+  = locale === 'zh' ? messages.zh.mottos.length : messages.en.mottos.length
+
+function getLang(currentPath: string, prefix = '') {
+  const cleanPrefix = (prefix ?? '')
+    .toString()
+    .trim()
+    .replace(/^\/+|\/+$/g, '')
+
+  if (!cleanPrefix) {
+    const match = currentPath?.match(/^\/([a-z]{2,})\/?/i)
+    return match ? match[1] : null
+  }
+
+  const regex = new RegExp(`^/${cleanPrefix}/([a-zA-Z]{2,})/?`)
+  const match = currentPath?.match(regex)
+
+  return match ? match[1] : null
+}
